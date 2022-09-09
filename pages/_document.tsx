47a4586cc251623247dbb89e45fb53b494579571
Rafał Kubacki <rafalkubacki7@gmail.com>
2022-09-09 15:06:10 +0200

    vercel vars

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          { process.env.NEXT_PUBLIC_VERCEL_ENV != "production" && <meta name="robots" content="noindex,nofollow" /> }
+        <Head data-env={process.env.NEXT_PUBLIC_VERCEL_ENV} data-url={process.env.NEXT_PUBLIC_VERCEL_URL}>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700&display=swap" rel="stylesheet" />
