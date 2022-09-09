7b94ead62164c04476a01ec21893bf12a4606eb1
Rafał Kubacki <rafalkubacki7@gmail.com>
2022-09-09 14:46:28 +0200

    seo files test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,9 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          { process.env.NEXT_PUBLIC_VERCEL_ENV != "production" && <meta name="robots" content="noindex,nofollow" /> }
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700&display=swap" rel="stylesheet" />
---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,31 @@
+import type { GetServerSideProps } from 'next'
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>${process.env.NEXT_PUBLIC_VERCEL_URL}</loc>
+     </url>
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const sitemap = generateSiteMap();
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
