b1ea511290284e03b5e520d5386892e2d26cab2c
Rafał Kubacki <rafalkubacki7@gmail.com>
2023-03-13 19:08:03 +0100

    gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TZBZK2N');`,
+            }}
+          ></Script>
           <link
             rel="preload"
             href="/fonts/Butler-Light.woff2"
@@ -18,6 +29,11 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZBZK2N" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
